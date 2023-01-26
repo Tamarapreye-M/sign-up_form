44ebda9ece6b20c783a8ea0cd8ee85d62bdc0eb6
Tamarapreye-M <preyemichael116@gmail.com>
2023-01-26 23:56:00 +0100

    structure setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,11 @@
 import React from "react";
 import "./App.css";
-import Input from "./components/Input";
+import Card from "./components/Card";
 
 const App = () => {
-	const inputArr = [
-		{ name: "firstName", label: "First Name" },
-		{ name: "lastName" },
-		{ name: "email" },
-		{ name: "password" },
-	];
 	return (
-		<div>
-			<h4>sign up</h4>
-			<h1>Craete an Account.</h1>
-			<p>Welcome to Onigs Where magic happens</p>
-			<div className="form-div">
-				<Input />
-			</div>
+		<div className="main-container">
+			<Card />
 		</div>
 	);
 };
