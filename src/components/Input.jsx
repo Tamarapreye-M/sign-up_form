0a560765a5f5817394fa6e6bb36cc43914e51a09
Tamarapreye-M <preyemichael116@gmail.com>
2023-01-28 23:06:27 +0100

    handled input values

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 
-const Input = ({ name, label, type }) => {
+const Input = ({
+	name,
+	label,
+	type,
+	handleEach,
+	showError,
+	id,
+	handleFormState,
+}) => {
+	console.log(id);
 	return (
 		<div className="form-div">
 			<label htmlFor="firstName">{label}</label>
 			<br />
-			<input type={type} />
+			<input
+				type={type}
+				name={name}
+				// className={showError && "error-state"}
+				onChange={handleFormState}
+			/>
+			{/* {showError && <p className="error-state">Enter valid input</p>} */}
 		</div>
 	);
 };
