6807ab08fb721967204bd27d07f34e902f6eb215
Tamarapreye-M <preyemichael116@gmail.com>
2023-02-04 14:03:10 +0100

    form validation partially complete

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
 const Input = (props) => {
-	let { name, type, handleFormState, className } = props;
+	let { name, type, handleFormState, className, maxLength } = props;
 	return (
 		<input
 			type={type}
 			name={name}
 			className={className}
 			onChange={handleFormState}
+			maxLength={maxLength}
 		/>
 	);
 };
