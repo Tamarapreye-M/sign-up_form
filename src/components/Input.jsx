e0ad418a8d2b5d8f9a0d9470d9ea4661ad5adb43
Tamarapreye-M <preyemichael116@gmail.com>
2023-02-07 13:28:46 +0100

    diable state for form validation

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Input = (props) => {
-	let { name, type, handleFormState, className, maxLength } = props;
+	const { name, type, handleFormState, className, maxLength } = props;
+
+	const [showPassword, setShowPassword] = useState(false);
+	const passwordIcon = (
+		<span class="material-symbols-outlined">visibility</span>
+	);
+
 	return (
-		<input
-			type={type}
-			name={name}
-			className={className}
-			onChange={handleFormState}
-			maxLength={maxLength}
-		/>
+		<div>
+			<input
+				type={type}
+				name={name}
+				className={className}
+				onChange={handleFormState}
+				maxLength={maxLength}
+			/>
+			{type === "password" && passwordIcon}
+		</div>
 	);
 };
 
