ce83d4596b329a81e8f12b1262b0e03d297754d0
Tamarapreye-M <preyemichael116@gmail.com>
2023-02-07 13:54:12 +0100

    proper components arrangements

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,45 +1,19 @@
 import React, { useState } from "react";
-import Input from "./Input";
-import Button from "./Button";
-const inputArr = [
-	{
-		name: "firstName",
-		label: "First Name",
-		type: "text",
-		id: ~~(Math.random() * 1000),
-		maxLength: "30",
-	},
-	{
-		name: "lastName",
-		label: "Last Name",
-		type: "text",
-		id: ~~(Math.random() * 1000),
-		maxLength: "30",
-	},
-	{
-		name: "email",
-		label: "Email",
-		type: "text",
-		id: ~~(Math.random() * 1000),
-		maxLength: "30",
-	},
-	{
-		name: "password",
-		label: "Password",
-		type: "password",
-		id: ~~(Math.random() * 1000),
-		maxLength: "12",
-	},
-];
+import Form from "./Form";
 
 const Card = () => {
+	// states
+	// form state
 	const [form, setForm] = useState({
 		firstName: { value: "", errorState: false, status: false },
 		lastName: { value: "", errorState: false, status: false },
 		email: { value: "", errorState: false, status: false },
 		password: { value: "", errorState: false, status: false },
 	});
+	// show password state
+	const [showPassword, setShowPassword] = useState(false);
 
+	// destructuring the form
 	const { firstName, lastName, email, password } = form;
 
 	const handleFormState = (ev) => {
@@ -92,38 +66,17 @@ const Card = () => {
 	};
 	console.log(form);
 
-	const allInput = inputArr.map((each, idx) => (
-		<div className="form-div" key={idx}>
-			<label htmlFor={each.name}>{each.label}</label>
-			<br />
-			<Input
-				{...each}
-				form={form}
-				setForm={setForm}
-				handleFormState={handleFormState}
-				className={form[each.name].errorState ? "error-state" : "success"}
-				maxLength={each.maxLength}
-			/>
-			{form[each.name].errorState ? (
-				<p className="error-state">{`Enter valid ${each.label.toLowerCase()}`}</p>
-			) : (
-				<p className="error-state bar">
-					{form[each.name].status && `maximum characters reached`}
-				</p>
-			)}
-		</div>
-	));
-	let disabledBtn = Object.values(form).some((each) => each.errorState);
-
 	return (
 		<div className="card">
 			<h4>sign up</h4>
 			<h1>Craete an Account.</h1>
 			<p>Welcome to Onigs Where magic happens</p>
-			<form className="form-container">
-				{allInput}
-				<Button content="sign in" disabled={disabledBtn} />
-			</form>
+			<Form
+				handleFormState={handleFormState}
+				form={form}
+				showPassword={showPassword}
+				setShowPassword={setShowPassword}
+			/>
 			<p>
 				{firstName.value}, {lastName.value}, {email.value}, {password.value}
 			</p>
