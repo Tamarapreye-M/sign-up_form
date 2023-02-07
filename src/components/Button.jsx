484fa7550f908c3f6b295e26c49f4658f452b333
Tamarapreye-M <preyemichael116@gmail.com>
2023-02-07 13:28:27 +0100

    props added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Button = ({ content }) => {
-	return <button type="submit">{content}</button>;
+const Button = ({ content, disabled }) => {
+	return (
+		<button
+			type="submit"
+			disabled={disabled}
+			className={disabled ? "disabled" : "able"}
+		>
+			{content}
+		</button>
+	);
 };
 
 export default Button;
